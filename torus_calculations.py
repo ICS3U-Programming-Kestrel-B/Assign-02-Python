03e4c41de431aabd6bb08bb217d209f1e545f0b7
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-10-03 13:45:38 +0000

    finished Python?

---
+++ torus_calculations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+ 
+# Created By: Kestrel Bryce
+# Date: Oct. 3, 2022
+# This program calculates the volume
+# and surface area of a torus
+ 
+ 
+# import torus_constants
+import math
+ 
+ 
+def main():
+    # input
+    print("This program calculates the volume")
+    print("and surface area of a torus.")
+    print("")
+    print("Note: the tube radius cannot be bigger than")
+    print("the radius of revolution, as that would result")
+    print("in an impossible shape :)")
+    print("")
+    tube_rad = int(input("Enter the torus's tube radius in inches: "))
+    rad_of_rev = int(input("Enter the torus's radius of revolution in inches: "))
+ 
+    # process
+    # calculating volume
+    volume = (math.pi * math.pi * 2 * 
+    (tube_rad * tube_rad) * rad_of_rev)
+
+    # calculating surface area
+    surface_area = (math.pi * math.pi * 4
+    * tube_rad * rad_of_rev)
+ 
+    # output
+    print("")
+    print("Your volume is {:,.2f} inches^3.".format(volume))
+    print("Your surface area is {:,.2f} inches^2.".format(surface_area))
+ 
+ 
+if __name__ == "__main__":
+    main()
---
+++ torus_constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Created By: Kestrel Bryce
+# Date: Oct. 3, 2022
+# This program contains the
+# constants for torus_calculations.py
+
+
+HST = 0.13
